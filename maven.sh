123e09b6dc03ca825a570b84bf83fe0f9f84d913
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-04-11 02:53:13 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,6 +1,8 @@
 # Install java 
+sudo hostnamectl set-hostname maven-setup
 yum update -y
 yum install java-17-amazon-corretto-devel -y
+java --version
 
 # Maven Installation
 wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
@@ -12,3 +14,23 @@ export M2_HOME=/root/apache-maven-3.9.9
 export PATH=$PATH:$M2_HOME/bin    
 
 source ~/.bash_profile  # to apply the changes without restarting
+
+
+#OR
+
+
+# Install java
+sudo hostnamectl set-hostname maven-setup
+yum update -y
+yum install java -y
+java --version
+
+# Maven Installation
+wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -xvzf apache-maven-3.9.9-bin.tar.gz 
+rm -rf apache-maven-3.9.9-bin.tar.gz
+
+export M2_HOME=/root/apache-maven-3.9.9
+env
+export PATH=$PATH:$M2_HOME/bin
+env
