226bfe57d131125e149b85e69243f229bcce8ebd
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-06-28 22:37:03 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -18,20 +18,30 @@ mvn --version
 
 #OR
 
+# Set hostname (optional)
+hostnamectl set-hostname maven-setup
 
-# Install java
-sudo hostnamectl set-hostname maven-setup
+# Update system and install Java 17
 yum update -y
-yum install java -y
-java --version
+yum install -y java-17-amazon-corretto
 
-# Maven Installation
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-tar -xvzf apache-maven-3.9.9-bin.tar.gz 
-rm -rf apache-maven-3.9.9-bin.tar.gz
+# Download and extract Maven
+wget https://archive.apache.org/dist/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -xzf apache-maven-3.9.9-bin.tar.gz
+rm -f apache-maven-3.9.9-bin.tar.gz
 
-export M2_HOME=/root/apache-maven-3.9.9
-env
-export PATH=$PATH:$M2_HOME/bin
-env
+# Rename and move Maven directory to /opt
+mv apache-maven-3.9.9 /opt/maven
+
+# Set environment variables
+cat <<EOF > /etc/profile.d/maven.sh
+export M2_HOME=/opt/maven
+export PATH=\$PATH:\$M2_HOME/bin
+EOF
+
+# Make script executable and apply immediately
+chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh  # run manually on CLI
+
+# Verify installation
 mvn --version
