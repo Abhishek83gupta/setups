e7e1df25e470d79b850c584fa6dbf6621ddd759a
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-06-29 12:47:08 +0530

    Create ubuntu-maven.sh

---
+++ ubuntu-maven.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set hostname (optional)
+hostnamectl set-hostname maven-setup
+
+# Update system and install Java 17 (OpenJDK)
+apt update -y
+apt install -y openjdk-17-jdk wget tar
+
+# Download and extract Maven
+wget https://archive.apache.org/dist/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -xzf apache-maven-3.9.9-bin.tar.gz
+rm -f apache-maven-3.9.9-bin.tar.gz
+
+# Rename and move Maven directory to /opt
+mv apache-maven-3.9.9 /opt/maven
+
+# Set environment variables
+cat <<EOF | tee /etc/profile.d/maven.sh > /dev/null
+export M2_HOME=/opt/maven
+export PATH=\$PATH:\$M2_HOME/bin
+EOF
+
+# Make script executable and apply immediately
+chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh   #run manually
+
+# Verify installation
+mvn --version
