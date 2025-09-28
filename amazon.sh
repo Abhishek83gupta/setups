f8b59074455b3f5586f989b3de964ae26234ea3a
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-09-28 18:38:13 +0530

    Create amazon.sh

---
+++ amazon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e 
+
+sudo yum update -y
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
+mv online-clone-amazon/* ./
+tail -f /var/log/httpd/access_log
