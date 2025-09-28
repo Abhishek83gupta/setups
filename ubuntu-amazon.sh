7f508bcc54edf5ddf7b203aec05e7d0b80855ba3
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-09-28 21:11:38 +0530

    Update ubuntu-amazon.sh

--- ubuntu-amazon.sh
+++ ubuntu-amazon.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 apt-get install apache2 git -y
 systemctl start apache2
 systemctl status apache2
-cd /var/www/html
+cd 
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
-mv online-clone-amazon/* .
+mv online-clone-amazon/* /var/www/html
 tail -f /var/log/apache2/access.log
