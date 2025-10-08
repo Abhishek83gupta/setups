5bfb500e12fc305d507181fbe7de0935eb58a51b
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-10-08 17:07:46 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 set -e 
 
-#STEP-1: INSTALLING GIT JAVA MAVEN 
-sudo apt update 
-apt-get install git maven -y
+#STEP-1: DOWNLOAD JAVA17 AND JENKINS
+sudo apt-get update -y 
+sudo apt-get install fontconfig openjdk-21-jre
+java -version
+# update-alternatives --config java
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > Debian) 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-#STEP-3: DOWNLOAD JAVA17 AND JENKINS
+#STEP-3: DOWNLOAD JENKINS
 sudo apt-get update
-sudo apt-get install fontconfig openjdk-17-jre
 sudo apt-get install jenkins
-update-alternatives --config java
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins
 systemctl status jenkins
-systemctl enable jenkins
