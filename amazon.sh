38c7a1f4679e7533103e5db8fc36bcb6ed5de0be
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-11-15 14:09:17 +0530

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -2,9 +2,10 @@
 set -e 
 
 sudo yum update -y
+sudo -i
 yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
-mv online-clone-amazon/* /var/www/html
+mv online-clone-amazon/* /var/www/html/
 tail -f /var/log/httpd/access_log
