d25c2a7b7ba7ff53331e79f4df6cc37f1413cc7a
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2025-11-16 13:26:03 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Update the system
+yum update -y
+
+# Docker
+yum install docker -y
+systemctl start docker
+systemctl status docker
+
+# Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+
+# minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo yum install iptables -y
+yum install conntrack -y
+minikube start --driver=docker --force
+minikube version  
+minikube status  
